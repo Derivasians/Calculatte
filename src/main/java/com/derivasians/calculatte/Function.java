d8eccaa36609b8cbae1953eac63eda5ef61616aa
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-18 10:21:54 -0400

    Created Function interface
    The function interface will allow users to create their own functions and pass them into Calculatte for calculations.

---
+++ src/main/java/com/derivasians/calculatte/Function.java
@@ -0,0 +1,14 @@
+package com.derivasians.calculatte;
+
+/**
+ * The Function interface for all other functions to be based off of.
+ */
+public interface Function {
+    /**
+     * Calculates the y-value for a given x-value of this function.
+     *
+     * @param x The x-value to find the y-value for.
+     * @return The y-value for the given x-value.
+     */
+    double f(double x);
+}
