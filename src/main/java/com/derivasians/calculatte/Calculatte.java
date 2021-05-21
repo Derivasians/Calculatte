1998e313de9e97f25df210bbe0b94faf62ba8baf
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-20 21:43:22 -0400

    Added volume of revolution calculation

--- src/main/java/com/derivasians/calculatte/Calculatte.java
+++ src/main/java/com/derivasians/calculatte/Calculatte.java
@@ -31,6 +31,35 @@ public class Calculatte {
     }
 
     /**
+     * Calculates the volume of revolution for the top function and bottom function
+     * of this instance of RevolutionSoup from a to b about the x-axis.
+     * of this instance of AreaSoup from a to b about the x-axis.
+     *
+     * @param a    The lower limit of integration.
+     * @param b    The upper limit of integration.
+     * @param axis The y/x value of the axis of rotation where 0 is about the x/y-axis.
+     * @return The volume of revolution.
+     */
+    public static double revolution(double a, double b, double axis, Function functionTop, Function functionBottom) {
+        // The top function with the axis offset squared.
+        Function squaredFunctionTop = new Squared() {
+            @Override
+            public double f(double x) {
+                return Math.pow(axis - functionTop.f(x), 2);
+            }
+        };
+        // The bottom function with the axis offset squared.
+        Function squaredFunctionBottom = new Squared() {
+            @Override
+            public double f(double x) {
+                return Math.pow(axis - functionBottom.f(x), 2);
+            }
+        };
+        // Split the volume of revolution formula into two separate integrals.
+        return Math.PI * (integrate(a, b, squaredFunctionTop) - integrate(a, b, squaredFunctionBottom));
+    }
+
+    /**
      * Main method for debugging.
      */
     public static void main(String[] args) {
@@ -40,6 +69,13 @@ public class Calculatte {
         System.out.println(a);
         // OUT: 3.9999997333333286
         // EXPECTED: 4
+
+        Function XSquared = x -> Math.pow(x, 2);
+        Function X = x -> 0;
+        double v = Calculatte.revolution(0, 2, 0, XSquared, X);
+        System.out.println(v);
+        // OUT: 20.10618963194089
+        // EXPECTED: 20.10618963194089
 
         // TODO: Make an in-house rounding system to correct errors in Simpson's rule.
     }
