dc8bcec06eeeadde0ae143615b2a71f7f7c8a334
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-25 07:50:35 -0400

    Added unit testing for tangentLine()
    Testing both the y-values and slope of the tangent line.

--- src/test/java/com/derivasians/calculatte/CalculatteTest.java
+++ src/test/java/com/derivasians/calculatte/CalculatteTest.java
@@ -6,6 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatteTest {
     @Test
+    void tangentLineOfXSquaredXIntercept() {
+        Function XSquared = x -> Math.pow(x, 2);
+        Function tangent = Calculatte.tangentLine(2, XSquared);
+        assertEquals(0, tangent.f(1));
+    }
+
+    @Test
+    void tangentLineOfXSquaredSlope() {
+        Function XSquared = x -> Math.pow(x, 2);
+        Function tangent = Calculatte.tangentLine(2, XSquared);
+        assertEquals(4, Calculatte.derivate(2, tangent));
+    }
+
+    @Test
     void integrateTwoXFrom0To2() {
         Function TwoX = x -> 2 * x;
         double area = Calculatte.integrate(0, 2, TwoX);
