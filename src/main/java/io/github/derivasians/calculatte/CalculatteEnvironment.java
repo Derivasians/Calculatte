7a5be1ea3d54f33f9441fe5551f9db831f6c1350
Matthew Okashita <okashita.matthew@gmail.com>
2021-07-06 15:30:00 -0400

    Fixed javadoc

--- src/main/java/io/github/derivasians/calculatte/CalculatteEnvironment.java
+++ src/main/java/io/github/derivasians/calculatte/CalculatteEnvironment.java
@@ -2,17 +2,17 @@ package io.github.derivasians.calculatte;
 
 public class CalculatteEnvironment {
     /**
-     * Sets the accuracy value for derivation calculations. The smaller the more accurate.
+     * Accuracy value for derivation calculations. The smaller the more accurate.
      */
     public static double H = 0.000000001;
 
     /**
-     * Sets the accuracy value for integration calculations. The larger the more accurate.
+     * Accuracy value for integration calculations. The larger the more accurate.
      */
     public static int N = 10000000;
 
     /**
-     * Sets the largest distance between the left and right limit before the limit does not
+     * Largest distance between the left and right limit before the limit does not
      * exist.
      */
     public static double LIMIT_TOLERANCE = 0.000000001;
